545a1db3bbbd15e26b9159b171566e6c4d2d4630
MatTheoo89 <matteobellomo89@gmail.com>
2022-10-20 13:17:34 +0200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+console.log('link collegato ok');
+
+/*
+L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+*/
+
+/*
+todo 1 - Struttura fissa HTML/CSS
+todo 2 - creare la griglia dinamicamente
+todo 3 - numerare quadratti
+todo 4 - Output
+*/
+
+
+/*
+! 1 - creare griglia dinamicamente
+
+*/
+
+
+/*
+! 2 - numerare quadratti
+*/
