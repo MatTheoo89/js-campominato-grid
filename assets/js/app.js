e99929e6fce89960621707c6ec4c3e1d1ea489d0
MatTheoo89 <matteobellomo89@gmail.com>
2022-10-20 14:50:45 +0200

    [js] dichiarazione dati iniziali

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,11 @@ console.log('link collegato ok');
 
 /*
 L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+
 Ogni cella ha un numero progressivo, da 1 a 100.
+
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 */
 
@@ -17,19 +20,20 @@ todo 4 - Output
 
 /*
 ! 1 - creare griglia dinamicamente
-  ?  quando EVENTO CLICK si genera la griglia di (.SQUARE)
-  
-  CONST Quante volte devo generare square
-  CONST EXponente (per non fare 10x10)
-  * clicco sul btn (EVENTO)
+  ? COSA DEVI FARE per generare la griglia?
 
-  * creo l'elemento square TANTE VOLTE 
-    CICLO
+  Quando premi sul PULSANTE vengono generati tanti quadrati, quanto il NUMERO moltiplicato per il NUMERO.
 
+  Ogni quadrato generato viene STAMPATO in pagina APPESO al CONTAINER
+
+  su ogni quadrato generato al suo interno dovrà avere il suo numero in ordine progressivo
 
 */
 
 
-/*
-! 2 - numerare quadratti
-*/
+// DATI PARTENZA
+const buttonPlay = document.querySelector('button'); // collego btn HTML ad variabile
+const container = document.querySelector('.container'); // collego container HTML ad variabile
+const elementForRow = 10; // poi prenderà il dato dalla select
+const nExponent = 2;
+//console.log(buttonPlay, elementForRow, nExponent);
